15fcf77fa3e5463489754b42c430fc1072c1979e
Zahra A <zahraa@Zahras-Air.home>
2022-12-08 18:26:04 -0500

    clean

--- models/entry.js
+++ models/entry.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+
 const carelySchema = new mongoose.Schema({
     date: String,
     title: String,
@@ -14,4 +15,5 @@ const carelySchema = new mongoose.Schema({
     moment: Boolean,
 })
 const Journal = mongoose.model('Journal', carelySchema)
+
 module.exports = Journal
