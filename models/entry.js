93ccaa66f11afbe7a527ac0381bc85eb20cec1b8
Eli Winslow <eliwinslow@Elis-MacBook-Air.local>
2022-12-08 14:02:04 -0800

    routesready-dbnotworking

---
+++ models/entry.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const carelySchema = new mongoose.Schema({
+    date: String,
+    title: String,
+    entry: String,
+    breath: Boolean,
+    body: Boolean,
+    breaks: Boolean,
+    grace: Boolean,
+    thoughts: Boolean,
+    connect: Boolean,
+    wins: Boolean,
+    help: Boolean,
+    moment: Boolean,
+})
+const Journal = mongoose.model('Journal', carelySchema)
+module.exports = Journal
--- server.js
+++ server.js
@@ -10,44 +10,45 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 //use public folder for static assets
 app.use(express.static('public'));
 
+
 //Port
 const PORT = process.env.PORT
+
 
 //Database
 // How to connect to the database either via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI;
+
+// Schema
+const Journal = require('./models/entry.js')
 
 
 //Middleware--------------------------------------------------
 
 // Routes
 
-app.get('/' , (req, res) => {
-  res.json({
-    test: 'meow'
+  app.get('/carely' , (req, res) => {
+    Journal.find({}, (err, allJournal) => {
+      res.json(allJournal)
+    })
   })
-});
 
-//app.post
+  app.post('/carely', (req, res) => {
+    Journal.create(req.body, (err, postJournal) => {
+      res.json(postJournal)
+    })
+  })
 
-//app.put
-
-//app.delete
-
-
-
-
-
-
+  app.put('/carely/:id', (req, res) => {
+    Journal.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedJournal) => { res.json(updatedJournal)
+    })
+  })
 
 
-
-
-
-
-
-
-
+  app.delete('/carely/:id', (req, res) => {
+    Journal.findByIdAndRemove(req.params.id, (err, deletedJournal) => { res.json(deletedJournal)
+    })
+  })
 
 
 
